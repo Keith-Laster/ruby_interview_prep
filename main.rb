38ab1b56789e57a6c1682424d657ee2efdbfa81a
Keith Laster <keith@Keiths-MBP.attlocal.net>
2022-06-19 16:42:55 -0500

    Added second prompt w/ answer

--- main.rb
+++ main.rb
@@ -42,3 +42,15 @@ for i in 1..src.length
   rev += src[src.length - i]
 end
 puts rev
+
+# Prompt2
+# Check to see if a string has duplicate letters
+
+def assertDuplicate(_initial_string)
+  for j in stringArr.length - 1
+    for i in stringArr.length - 1
+      return true if stringArr[j] == stringArr[i] && i != j
+    end
+  end
+  false
+end

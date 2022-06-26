33d996f1f294a10e6d89788bd476d4f7ec7ce390
Keith Laster <lasterk9@gmail.com>
2022-06-25 21:12:01 -0500

    added a loop to prompt 3 v.3

--- prompt3.rb
+++ prompt3.rb
@@ -11,3 +11,45 @@ end
 
 puts palendrome_hunter('poop')
 puts palendrome_hunter('butts')
+
+puts '......'
+
+# prompt 3.1
+# Make the above method at least 2x as fast.
+
+def palendrome_hunter2(string1)
+  first_half = ''
+  second_half = ''
+  for i in 0..((string1.length - 1) / 2)
+    first_half += string1[i]
+  end
+  for i in (string1.length / 2)..(string1.length - 1)
+    second_half += string1[i]
+  end
+  first_half == second_half.reverse
+end
+
+puts palendrome_hunter2('poop')
+puts palendrome_hunter2('butts')
+
+puts '......................................'
+
+def palendrome_hunter3(string)
+  frontstring = ''
+  endstring = ''
+
+  0.upto((string.length - 1) / 2) do |i|
+    frontstring += string[0 + i]
+    endstring += string[(string.length - 1) - i]
+    if frontstring == endstring
+      i += 1
+      puts i
+    else
+      puts 'nahhh'
+      break
+    end
+    puts 'true'
+  end
+end
+palendrome_hunter3('poop')
+palendrome_hunter3('butts')

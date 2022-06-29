928565346701b8d22c2f34471990db484ae287d1
Keith Laster <lasterk9@gmail.com>
2022-06-28 23:26:56 -0500

    solved Fizzbuzz by adding 3 conditionals.

--- prompt4.rb
+++ prompt4.rb
@@ -6,8 +6,13 @@ def fizzbuzz
   1.upto(99) do
     x += 1
     arrayy << x
+  end
 
-    end
+  arrayy.each do |n|
+    puts "#{n} - fizz" if !(n % 5 == 0) && (n % 3 == 0)
+    puts "#{n} - buzz" if !(n % 3 == 0) && (n % 5 == 0)
+    puts "#{n} - fizzbuzz" if (n % 3 == 0) & (n % 5 == 0)
   end
 end
+
 fizzbuzz

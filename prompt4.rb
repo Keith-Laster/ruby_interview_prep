2dfb5a34139deee4a6f26d0d3e17c2e661fb673f
Keith Laster <lasterk9@gmail.com>
2022-06-28 23:17:20 -0500

    added array to fizzbuzz challenge

---
+++ prompt4.rb
@@ -0,0 +1,13 @@
+# return "fiz" if divisible by 3, return "buzz" if divisable by 4, return "fizbuzz if divisable by both"
+
+def fizzbuzz
+  x = 1
+  arrayy = [x]
+  1.upto(99) do
+    x += 1
+    arrayy << x
+
+    end
+  end
+end
+fizzbuzz

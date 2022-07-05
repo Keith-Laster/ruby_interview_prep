6fba11fa68cfde18b028c007cf84471037e69b4c
Keith Laster <lasterk9@gmail.com>
2022-07-05 18:47:40 -0500

    new tactic on prompt5 under construction.

--- prompt5.rb
+++ prompt5.rb
@@ -1,48 +1,16 @@
 # return a string in alphebetical order w/o using built in 'sort' method
 
-def alphebetizer
-  string1 = 'larry'
+string1 = 'larry'
+final_array = []
 
-  string_array = string1.chars
+string1_array = string1.chars
+p string1_array
 
-  string_array.each do |c|
-    p alphabet[c]
-  end
-
-  #   for i in 0..(string1.length - 1)
-  #     puts string1[i]
-  #   end
-
-  alphabet = {
-    'a' => 1,
-    'b' => 2,
-    'c' => 3,
-    'd' => 4,
-    'e' => 5,
-    'f' => 6,
-    'g' => 7,
-    'h' => 8,
-    'i' => 9,
-    'j' => 10,
-    'k' => 11,
-    'l' => 12,
-    'm' => 13,
-    'n' => 14,
-    'o' => 15,
-    'p' => 16,
-    'q' => 17,
-    'r' => 18,
-    's' => 19,
-    't' => 20,
-    'u' => 21,
-    'v' => 22,
-    'w' => 23,
-    'x' => 24,
-    'y' => 25,
-    'z' => 26
-  }
-
-  values = alphabet.values
+string1_array.each do |c|
+    if c >= string1_array[0, 1, 2, 3, 4, 5]
+       final_array <<
+    else
+        p "test"
+    end
 end
-
-alphebetizer
+end

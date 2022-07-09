1c11183d5c34512ef0e0d68721b4b6e9101ec8eb
Keith Laster <lasterk9@gmail.com>
2022-07-09 16:00:04 -0500

    introduced new tactic of a for loop inside of a for loop.

--- prompt5.rb
+++ prompt5.rb
@@ -6,8 +6,16 @@ final_array = []
 string1_array = string1.chars
 p string1_array
 
-string1_array.each do |c|
-while c >= string1_array[0, 1, 2, 3, 4, 5]
-       final_array <<
+# string1_array.each do |c|
+# while c >= string1_array[0, 1, 2, 3, 4, 5]
+#        final_array <<
+
+p 'b' > 'a'
+
+for j in 0..string1_array.length - 1
+  for i in 0..string1_array.length - 1
+    final_array << string1_array[j] if string1_array[j] <= string1_array[i]
+  end
 end
-end
+
+p final_array

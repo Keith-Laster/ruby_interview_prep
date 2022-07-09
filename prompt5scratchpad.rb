f4b1399c8f8d881dd9cb4bea9c1b750a81d76c20
Keith Laster <lasterk9@gmail.com>
2022-07-09 18:32:34 -0500

    added Kyle's 'winning_char' method in prompt5scratchpad.

---
+++ prompt5scratchpad.rb
@@ -0,0 +1,16 @@
+string = 'taco'
+string_array = string.chars
+final_array
+winning_char = ''
+
+#look up syntax for while loops
+
+while string_array != []
+for i in 0..string - 1
+winning_char = string_array[0]
+if string_array[i] < winning_char 
+    winning_char = string_array[i]
+end
+final_array << winning_char
+string_array[#subtract winning array]
+end

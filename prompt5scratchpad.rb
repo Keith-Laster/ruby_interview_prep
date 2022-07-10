d360e66099fc4f8aafcd15a322dd6add7a778210
Keith Laster <lasterk9@gmail.com>
2022-07-10 17:25:02 -0500

    zeroing in on solution, nil class error remains.

--- prompt5scratchpad.rb
+++ prompt5scratchpad.rb
@@ -1,16 +1,16 @@
 string = 'taco'
 string_array = string.chars
-final_array
+final_array = []
 winning_char = ''
 
-#look up syntax for while loops
+# look up syntax for while loops
 
 while string_array != []
-for i in 0..string - 1
-winning_char = string_array[0]
-if string_array[i] < winning_char 
-    winning_char = string_array[i]
+  for i in 0..string.length - 1
+    winning_char = string_array[0]
+    winning_char = string_array[i] if string_array[i] < winning_char
+    final_array << winning_char
+    string_array.delete(winning_char)
+  end
 end
-final_array << winning_char
-string_array[#subtract winning array]
-end
+p final_array

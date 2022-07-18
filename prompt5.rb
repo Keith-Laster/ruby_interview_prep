fced35a1bdaf97a21f4a9ac57259f5f505c8871e
Keith Laster <lasterk9@gmail.com>
2022-07-17 20:24:22 -0500

    began construction of insertion method

--- prompt5.rb
+++ prompt5.rb
@@ -1,21 +1,11 @@
 # return a string in alphebetical order w/o using built in 'sort' method
 
-string1 = 'larry'
-final_array = []
+string = 'taco'
+arr = string.chars
 
-string1_array = string1.chars
-p string1_array
-
-# string1_array.each do |c|
-# while c >= string1_array[0, 1, 2, 3, 4, 5]
-#        final_array <<
-
-p 'b' > 'a'
-
-for j in 0..string1_array.length - 1
-  for i in 0..string1_array.length - 1
-    final_array << string1_array[j] if string1_array[j] <= string1_array[i]
+for i in 0..string.length - 1
+  j = i
+  while   j > 0 and arr[j-1] < arr[j]
+    
+    
   end
-end
-
-p final_array
--- prompt5scratchpad.rb
+++ prompt5scratchpad.rb
@@ -3,16 +3,13 @@ string_array = string.chars
 final_array = []
 winning_char = ''
 
-# look up syntax for while loops
+# while string_array != []
+for i in 0..string.length - 1
+  winning_char = string_array[0]
+  next unless string_array[i] < winning_char
 
-while string_array != []
-  for i in 0..string.length - 1
-    winning_char = string_array[0]
-    if string_array[i] < winning_char
-      winning_char = string_array[i] 
-    final_array << winning_char
-    string_array.delete(winning_char)
-  end
+  winning_char = string_array[i]
+  final_array << winning_char
 end
-end
+# end
 p final_array

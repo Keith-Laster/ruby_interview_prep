1b3133cd0049b81437968a5a6bbf10154ab269c3
Keith Laster <lasterk9@gmail.com>
2022-08-04 16:24:55 -0500

    updated scratchpad w/ solution for 4x4 snail sort.

--- scratchpad.rb
+++ scratchpad.rb
@@ -5,27 +5,43 @@ def snail(arr)
 
   n = array.length - 1
 
+  # first down
   n.times do |i|
     output << array[i + 1][n]
-    i += 1 if array[i + 1] != array[n - 1]
   end
-
   p output
 
+  # first left
   1.upto(n) do |i|
     output << array[n][n - i]
   end
-
   p output
 
-  0.upto(n - 1) do |i|
-    output << array[n - 1][i]
+  # first up
+  1.upto(n - 1) do |i|
+    output << array[n - i][0]
   end
+  p output
 
+  # second right
+  1.upto(n - 1) do |i|
+    output << array[1][i]
+  end
+  p output
+
+  # second down
+  1.upto(n - 2) do |i|
+    output << array[i + 1][n - 1]
+  end
+  p output
+
+  # second left
+  1.upto(n - 2) do |i|
+    output << array[i + 1][n - 1 - i]
+  end
   p output
 end
 
-snail([[1, 2, 3, 4],
-       [5, 6, 7, 8],
-       [9, 10, 11, 12],
-       [13, 14, 15, 16]])
+snail([[1, 2, 3],
+       [4, 5, 6],
+       [7, 8, 9]])

b17098a05d5f01ee0092de4b9d1e68d6bc44dfd4
Keith Laster <lasterk9@gmail.com>
2022-08-08 15:52:35 -0500

    begain construction on a nested loop solution for snail sort

--- prompt6_snail_sort.rb
+++ prompt6_snail_sort.rb
@@ -2,22 +2,33 @@
 # Expected output = 1,2,3,6,9,8,7,4,5
 
 def snail(data_set)
-  array_length = data_set.length - 1
-
+  n = data_set.length - 1
   output = []
-  j = 0
-  horizontal = []
-  vertical = []
+  horizontal_left_right = []
+  horizontal_right_left = []
+  vertical_up_down = []
+  vertical_down_up = []
+  k = 0
 
-  for i in 0..array_length do
-    for j in 0..array_length do
-      horizontal << data_set[0][j]
+  for i in 0..n do
+    for j in 0..n do
+      # next unless (i == 0 || j == n) || (i == n && j != n)
+      horizontal_left_right << data_set[i][j] if i == 0
+      vertical_up_down << data_set[i][j] if j == n && i != 0
+      horizontal_right_left << data_set[i][j] if i == n && j != n
+      vertical_down_up << data_set[i][j] if i != 0 && i != n && j = 0
     end
-
+    k += 1
   end
+  output << horizontal_left_right
+  output << vertical_up_down
+  output << horizontal_right_left.reverse
+  output << vertical_down_up.reverse
   p output
 end
 
 snail([[0, 1, 2],
        [3, 4, 5],
        [6, 7, 8]])
+
+#  [0][0], [0][1], [0][2], [1][2], [2][2], [2][1], [2][0]
